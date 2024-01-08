8dc40a53dcb1fd15824772f136cff1a28efe268c
sevvalislekter <146951024+sevvalislekter@users.noreply.github.com>
2024-01-08 17:47:35 +0300

    parantez oluşturma

---
+++ main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        if n == 0:
+            return []
+
+        result = []
+        self.helper(n, n, '', result)
+        return result
+
+    def helper(self, l, r, item, result):
+        if r < l:
+            return
+        if l == 0 and r == 0:
+            result.append(item)
+        if l > 0:
+            self.helper(l - 1, r, item + '(', result)
+        if r > 0:
+            self.helper(l, r - 1, item + ')', result)
+
+a=Solution()
+print(a.generateParenthesis(3))
